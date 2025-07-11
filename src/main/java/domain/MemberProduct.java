2a41e9171fd4784f8edd64a2afc97545285cd7d6
sunjaewow <chltjswo789@naver.com>
2025-07-11 12:59:22 +0900

    대리키로 해결

--- src/main/java/domain/MemberProduct.java
+++ src/main/java/domain/MemberProduct.java
@@ -4,15 +4,18 @@ import jakarta.persistence.*;
 import practice.Member;
 
 @Entity
-@IdClass(MemberProductId.class)//복합 키를 사용하려면 식별자 클래스가 필요함.
+//@IdClass(MemberProductId.class)//복합 키를 사용하려면 식별자 클래스가 필요함.
 public class MemberProduct {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "MEMBER_PRODUCT_ID")
+    private Long id;
+
     @ManyToOne
     @JoinColumn(name = "MEMBER_ID")
     private Member member;
 
-    @Id
     @ManyToOne
     @JoinColumn(name = "PRODUCT_ID")
     private Product product;
