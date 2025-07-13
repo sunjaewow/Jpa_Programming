6b6fd0244d61e99769ffd68652858c4a45aa87a1
sunjaewow <chltjswo789@naver.com>
2025-07-13 14:40:40 +0900

    @AttributeOverride 추가

--- src/main/java/domain/mapped/User.java
+++ src/main/java/domain/mapped/User.java
@@ -1,8 +1,11 @@
 package domain.mapped;
 
+import jakarta.persistence.AttributeOverride;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 
 @Entity
+@AttributeOverride(name = "id", column = @Column(name = "MEMBER_ID"))//부모에게 상속받은 id속성의 컬럼명을 변경
 public class User extends BaseEntity{
     private String email;
 }
