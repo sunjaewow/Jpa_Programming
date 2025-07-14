f80a8cffd80c96da001a80c4f30d77f4405e8bb1
sunjaewow <chltjswo789@naver.com>
2025-07-14 11:14:37 +0900

    일대일 식별 관계

---
+++ src/main/java/식별일대일/Board.java
@@ -0,0 +1,17 @@
+package 식별일대일;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Board {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "BOARD_ID")
+    private Long id;
+
+    private String title;
+
+    @OneToOne(mappedBy = "board")
+    private BoardDetail boardDetail;
+}
---
+++ src/main/java/식별일대일/BoardDetail.java
@@ -0,0 +1,17 @@
+package 식별일대일;
+
+import jakarta.persistence.*;
+
+@Entity
+public class BoardDetail {
+
+    @Id
+    private Long boardId;
+
+    @MapsId//BoardDetail.boardId 매핑
+    @OneToOne
+    @JoinColumn(name = "BOARD_ID")
+    private Board board;
+
+    private String content;
+}
