e7522a70aa30eaa119cbf968e0474450b89f5c0c
sunjaewow <chltjswo789@naver.com>
2025-07-15 16:33:45 +0900

    값 타입 공유 참조를 막기위한 불변 객체 생성 및 ElementCollection

--- src/main/java/embeddedtype/Address.java
+++ src/main/java/embeddedtype/Address.java
@@ -2,15 +2,28 @@ package embeddedtype;
 
 import jakarta.persistence.Embeddable;
 import jakarta.persistence.Embedded;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Embeddable
+@NoArgsConstructor
 public class Address {
 
+    @Getter
     private String street;
 
+    @Getter
     private String city;
 
+    @Getter
     private String state;
+
+    public Address(String street, String city, String state) {
+        this.street = street;
+        this.city = city;
+        this.state = state;
+    }
+
 
     @Embedded
     private Zipcode zipcode;
--- src/main/java/embeddedtype/Member.java
+++ src/main/java/embeddedtype/Member.java
@@ -2,6 +2,11 @@ package embeddedtype;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 @Entity
 public class Member {
 
@@ -15,11 +20,22 @@ public class Member {
 
     //만약 주소하나를 더 추가하고싶으면 prviate Address homeAddress? -> 매핑하는 칼럼명이 중복이 됨 그래서 속성 재정의를 해야함.
 
-    @Embedded
-    @AttributeOverrides({
-            @AttributeOverride(name = "city", column = @Column(name = "COMPANY_CITY")),
-            @AttributeOverride(name = "street", column = @Column(name = "COMPANY_STREET")),
-            @AttributeOverride(name = "state", column = @Column(name = "COMPANY_STATE"))
-    })
-    private Address companyAddress;//사용하면 어노테이션을 너무 많이 사용해서 지저분해짐.
+//    @Embedded
+//    @AttributeOverrides({
+//            @AttributeOverride(name = "city", column = @Column(name = "COMPANY_CITY")),
+//            @AttributeOverride(name = "street", column = @Column(name = "COMPANY_STREET")),
+//            @AttributeOverride(name = "state", column = @Column(name = "COMPANY_STATE"))
+//    })
+//    private Address companyAddress;//사용하면 어노테이션을 너무 많이 사용해서 지저분해짐.
+
+    @ElementCollection
+    @CollectionTable(name = "FAVORITE_FOODS",
+            joinColumns = @JoinColumn(name = "MEMBER4_ID"))
+    @Column(name = "FOOD_NAME")
+    private Set<String> favoriteFoods = new HashSet<>();
+
+    @ElementCollection
+    @CollectionTable(name = "ADDRESS",
+            joinColumns = @JoinColumn(name = "MEMBER4_ID"))
+    private List<Address> addressHistory = new ArrayList<>();
 }
