dafa906348e25f21a68b3c310e6128d7e29bb8e2
sunjaewow <chltjswo789@naver.com>
2025-07-15 16:04:58 +0900

    @AttributeOverride 추가

--- src/main/java/embeddedtype/Member.java
+++ src/main/java/embeddedtype/Member.java
@@ -1,7 +1,6 @@
 package embeddedtype;
 
 import jakarta.persistence.*;
-import org.springframework.stereotype.Component;
 
 @Entity
 public class Member {
@@ -14,4 +13,13 @@ public class Member {
     @Embedded
     private Address address;
 
+    //만약 주소하나를 더 추가하고싶으면 prviate Address homeAddress? -> 매핑하는 칼럼명이 중복이 됨 그래서 속성 재정의를 해야함.
+
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name = "city", column = @Column(name = "COMPANY_CITY")),
+            @AttributeOverride(name = "street", column = @Column(name = "COMPANY_STREET")),
+            @AttributeOverride(name = "state", column = @Column(name = "COMPANY_STATE"))
+    })
+    private Address companyAddress;//사용하면 어노테이션을 너무 많이 사용해서 지저분해짐.
 }
